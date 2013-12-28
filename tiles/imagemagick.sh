8f3aba5ac1cb1071e9b022b554861d6e3ba3be1b
Micho <github@michael-kreil.de>
2013-12-28 23:49:40 +0100

    tile update

---
+++ tiles/imagemagick.sh
@@ -0,0 +1,7 @@
+convert "30c3-map_v0.2-beta3-tiles.png" -filter Box -resize 256x256 -crop 256x256 -set filename:tile "%[fx:page.x/256]/%[fx:page.y/256]" +repage +adjoin "0/%[filename:tile].jpg"
+convert "30c3-map_v0.2-beta3-tiles.png" -filter Box -resize 512x512 -crop 256x256 -set filename:tile "%[fx:page.x/256]/%[fx:page.y/256]" +repage +adjoin "1/%[filename:tile].jpg"
+convert "30c3-map_v0.2-beta3-tiles.png" -filter Box -resize 1024x1024 -crop 256x256 -set filename:tile "%[fx:page.x/256]/%[fx:page.y/256]" +repage +adjoin "2/%[filename:tile].jpg"
+convert "30c3-map_v0.2-beta3-tiles.png" -filter Box -resize 2048x2048 -crop 256x256 -set filename:tile "%[fx:page.x/256]/%[fx:page.y/256]" +repage +adjoin "3/%[filename:tile].jpg"
+convert "30c3-map_v0.2-beta3-tiles.png" -filter Box -resize 4096x4096 -crop 256x256 -set filename:tile "%[fx:page.x/256]/%[fx:page.y/256]" +repage +adjoin "4/%[filename:tile].jpg"
+convert "30c3-map_v0.2-beta3-tiles.png" -filter Box -resize 8192x8192 -crop 256x256 -set filename:tile "%[fx:page.x/256]/%[fx:page.y/256]" +repage +adjoin "5/%[filename:tile].jpg"
+convert "30c3-map_v0.2-beta3-tiles.png" -crop 256x256 -set filename:tile "%[fx:page.x/256]/%[fx:page.y/256]" +repage +adjoin "6/%[filename:tile].jpg"
